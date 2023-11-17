deea92be44bd59a4564219acb8597c84d060db92
hawk198723 <hawk198723@gmail.com>
2023-11-17 14:56:29 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+import { Routes, RouterModule } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { CounterComponent } from './counter/counter.component';
+// import { FetchDataComponent } from './fetch-data/fetch-data.component';
+// import { EmployeeComponent } from './employee/employee.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  // { path: 'employee', component: EmployeeComponent },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'counter', component: CounterComponent },
+  // { path: 'fetch-data', component: FetchDataComponent },
+  { path: '**', redirectTo: 'employee' },
+];
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
