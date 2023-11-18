a8705285546f0c0446cf26e470e1878261d41dfc
hawk198723 <hawk198723@gmail.com>
2023-11-17 17:26:18 -0800

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +22,15 @@ import { FetchdataService } from './fetchdata.service';
     FetchDataComponent,
     EmployeeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [
     { provide: 'BASE_URL', useFactory: getBaseUrl },
     FetchdataService,
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  public employeeList: Employee[] = [];
+  showEditor = true;
+  myName: string = '';
+  newEmployee: Employee = new Employee();
+  findEmployee: Employee = new Employee();
 
+  constructor(private dataService: EmployeeService) {
+    // this.newEmployee = new Employee();
+    // this.findEmployee = new Employee();
+    // this.findEmployee.name = '';
+  }
+
+  ngOnInit() {
+    this.getAll();
+  }
+
+  employeeSearch() {
+    console.log('In employeeSearch ');
+    if (this.findEmployee.name.trim()) return;
+    this.dataService.searchEmployees(this.findEmployee.name).subscribe(
+      (data: Employee[]) => {
+        console.log('found employees ' + data);
+        this.employeeList = data;
+      },
+      (error) => {
+        console.log('could not get Employees', error);
+        this.employeeList = [];
+      }
+    );
+  }
+
+  getAll() {
+    console.log('In getAll:');
+    this.dataService.getAll().subscribe(
+      (data: Employee[]) => {
+        console.log('found employees ' + data);
+        this.employeeList = data;
+      },
+      (error) => {
+        console.log('could not get Employees', error);
+        this.employeeList = [];
+      }
+    );
+  }
+
+  public addEmployee(item: Employee) {
+    // console.log("In addEmployee: " + this.newEmployee);
+    this.dataService.addEmployee(this.newEmployee).subscribe((employee) => {
+      this.employeeList.push(employee);
+      console.dir(employee);
+    });
+    // console.dir(employeeId);
+  }
+
+  public updateEmployee(item: Employee) {
+    console.dir(item);
+    // console.log("In updateEmployee: " + item);
+    this.dataService.updateEmployee(item).subscribe((employee) => {
+      // this.employeeList.push(employee);
+      console.dir(employee);
+      this.getAll();
+    });
+    // console.log("In updateEmployee:");
+  }
+
+  public deleteEmployee(employee: Employee) {
+    console.log('In deleteEmployee: ' + employee.id);
+    // this.dataService.deleteEmployee(employee.id);
+    this.employeeList = this.employeeList.filter((h) => h !== employee);
+    this.dataService.deleteEmployee(employee).subscribe();
+  }
+
+  findEmployeeById() {
+    this.dataService
+      .getEmployeeNo404({ id: this.findEmployee.id })
+      .subscribe((e) => {
+        if (e !== undefined) this.findEmployee = e;
+      });
+  }
+
+  getEmployeeById() {
+    this.dataService.getEmployee(this.findEmployee.id).subscribe(
+      (e) => {
+        if (e !== undefined) this.findEmployee = e;
+      },
+      (error) => {
+        console.log('could not get Employees', error);
+      }
+    );
+  }
+}
+
+export class Employee {
+  public id: number = 0;
+  public name: string = '';
+  public gender: string = '';
+  public departmentId: number = 0;
+  public salary: number = 0;
 }
